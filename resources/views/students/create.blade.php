1e94cb0b8a616343252a191b6e3bb27c9b0878c4
sih han wu <sword801128@gmail.com>
2018-06-29 13:57:40 +0800

    add icons

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -11,22 +11,27 @@
         <form action="" method="post" class="col s12">
             {{ csrf_field() }}
             <div class="row">
-                <div class="input-field col s6">
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">account_circle</i>
                     <input id="name" name="student" type="text" class="validate">
                     <label for="name">Name</label>
                 </div>
 
-                <div class="input-field col s6">
-                    <input id="email" name="email" type="text" class="validate">
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">email</i>
+                    <input id="email" name="email" type="email" class="validate">
+                    <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     <label for="email">Email</label>
                 </div>
 
-                <div class="input-field col s6">
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">people</i>
                     <input id="facebook" name="facebook" type="text" class="validate">
                     <label for="facebook">Facebook</label>
                 </div>
 
-                <div class="input-field col s6">
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">phone_iphone</i>
                     <input id="mobile" name="mobile" type="text" class="validate">
                     <label for="mobile">Mobile</label>
                 </div>
